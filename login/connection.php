e7b689534f53d2e9e74c1341f5fa2f45426a52bb
vincpinas <70635677+vincpinas@users.noreply.github.com>
2021-04-07 11:59:26 +0200

    :blue_heart: connection fix

--- login/connection.php
+++ login/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "login";
+$dbhost = "127.0.0.1";
+$dbuser = "c6446dhuis";
+$dbpass = "tb@aGiEp3JD";
+$dbname = "c6446dhuis";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
